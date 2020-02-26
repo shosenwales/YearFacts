fe59ec9bec829e32847e6f7c35dfd3ff3dfbc113
shuaib <wales149@gmail.com>
2020-02-25 22:46:51 -0800

    i think it works

---
+++ main.js
@@ -0,0 +1,19 @@
+let fact = document.querySelector('#fact');
+let factText = document.querySelector('#factText');
+
+let yearInput = document.querySelector('#yearInput');
+yearInput.addEventListener('input', getFactFetch);
+
+function getFactFetch(){
+    let year = yearInput.value;
+
+    fetch('http://numbersapi.com/'+year+'/year')
+        .then(response => response.text())
+        .then(data => {
+           if(year != ''){
+               fact.style.display = 'block';
+               factText.innerText = data;
+           }
+        })
+        .catch(err => console.log(err));
+}
