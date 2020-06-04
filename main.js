302ea03cf54172c407fce3174d37e9c5ee9f9d72
shuaib <wales149@gmail.com>
2020-06-04 15:38:39 -0700

    fixed cors issues

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-let fact = document.querySelector('#fact');
-let factText = document.querySelector('#factText');
+let fact = document.querySelector("#fact");
+let factText = document.querySelector("#factText");
 
-let yearInput = document.querySelector('#yearInput');
-yearInput.addEventListener('input', getFactFetch);
+let yearInput = document.querySelector("#yearInput");
+yearInput.addEventListener("input", getFactFetch);
 
-function getFactFetch(){
-    let year = yearInput.value;
+function getFactFetch() {
+  let year = yearInput.value;
 
-    fetch('http://numbersapi.com/'+year+'/year')
-        .then(response => response.text())
-        .then(data => {
-           if(year != ''){
-               fact.style.display = 'block';
-               factText.innerText = data;
-           }
-        })
-        .catch(err => console.log(err));
+  fetch(
+    "https://cors-anywhere.herokuapp.com/http://numbersapi.com/" +
+      year +
+      "/year"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      if (year != "") {
+        fact.style.display = "block";
+        factText.innerText = data;
+      }
+    })
+    .catch((err) => console.log(err));
 }
